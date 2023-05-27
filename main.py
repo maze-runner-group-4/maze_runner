9ba88259f1f4015bd7d0b3695f6968629a478e3c
abdulkareemAbunabhan <zman17881@gmail.com>
2023-05-27 10:31:31 +0000

    feat:player movement

--- main.py
+++ main.py
@@ -30,21 +30,21 @@ class MazeGame:
         for row in range(len(maze)):
             for col in range(len(maze[row])):
                 if maze[row][col] == "P":
-                    return (row, col)  
+                    return [row, col]  
         return None  
 
     def find_goal(self, maze):
         for row in range(len(maze)):
             for col in range(len(maze[row])):
                 if maze[row][col] == "G":
-                    return (row, col)  
+                    return [row, col]  
         return None  
     
     def find_treasure(self, maze):
         for row in range(len(maze)):
             for col in range(len(maze[row])):
                 if maze[row][col] == "T":
-                    return (row, col)   
+                    return [row, col]   
         return None  
     
     def draw_treasure(self):
@@ -63,8 +63,46 @@ class MazeGame:
                     wall_cell_image = pygame.image.load(os.path.join("Assets","wall_cell.png"))
                     wall_cell = pygame.transform.scale(wall_cell_image,(self.cell_width,self.cell_height))
                     self.window.blit(wall_cell,(column * self.cell_width, row * self.cell_height))
-
     
+    def draw_player(self):
+        player_rect = pygame.Rect(
+            self.player_pos[1] * self.cell_width + self.border_size,
+            self.player_pos[0] * self.cell_height + self.border_size,
+            self.cell_width - self.border_size * 2,
+            self.cell_height - self.border_size * 2
+        )
+        pygame.draw.rect(self.window, RED, player_rect)
+
+    def draw_goal(self):
+        goal_rect = pygame.Rect(
+            self.goal[1] * self.cell_width + self.border_size,
+            self.goal[0] * self.cell_height + self.border_size,
+            self.cell_width - self.border_size * 2,
+            self.cell_height - self.border_size * 2
+        )
+        pygame.draw.rect(self.window, GREEN, goal_rect)
+    def move_player(self, direction):
+        new_row = self.player_pos[0]
+        new_col = self.player_pos[1]
+
+        if direction == "up":
+            new_row -= 1
+        elif direction == "down":
+            new_row += 1
+        elif direction == "left":
+            new_col -= 1
+        elif direction == "right":
+            new_col += 1
+
+        if self.is_valid_move(new_row, new_col):
+            self.player_pos = (new_row, new_col)
+
+    def is_valid_move(self, row, col):
+        if row < 0 or row >= len(self.maze) or col < 0 or col >= len(self.maze[0]):
+            return False
+        if self.maze[row][col] == "X":
+            return False
+        return True
     
     def run(self):
         running = True
@@ -72,21 +110,21 @@ class MazeGame:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                # elif event.type == pygame.KEYDOWN:
-                #     if event.key == pygame.K_UP:
-                #         self.move_player("up")
-                #     elif event.key == pygame.K_DOWN:
-                #         self.move_player("down")
-                #     elif event.key == pygame.K_LEFT:
-                #         self.move_player("left")
-                #     elif event.key == pygame.K_RIGHT:
-                #         self.move_player("right")
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.move_player("up")
+                    elif event.key == pygame.K_DOWN:
+                        self.move_player("down")
+                    elif event.key == pygame.K_LEFT:
+                        self.move_player("left")
+                    elif event.key == pygame.K_RIGHT:
+                        self.move_player("right")
 
             self.window.fill(BLACK)
 
             self.draw_maze()
-            # self.draw_player()
-            # self.draw_goal()
+            self.draw_player()
+            self.draw_goal()
             self.draw_treasure()
             # if self.check_find_goal():
             #     print("Congratulations! You Win!")
