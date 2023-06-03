6496aab89b03ff9d2d5469e2ccbe452cae792bef
abdullah <abdullahshagnoba@gmail.com>
2023-06-03 14:48:21 +0300

    feat: dodge the monsters map 2 and its functions done

--- dodge_the_monsters.py
+++ dodge_the_monsters.py
@@ -1,7 +1,7 @@
 import os
 import pygame
 from main import MazeGame
-# from maze_maps import Maze_maps
+from maze_maps import Maze_maps
 from spritesheet_test import list_of_frames_blue_monster_back,list_of_frames_red_monster_back,list_of_frames_hearts,list_of_frames_red_monster_front,list_of_frams_blue_monster_front,animation_list_left,animation_list_right
 BLACK = (0, 0, 0)
 indigo = "indigo"
@@ -11,41 +11,73 @@ class Dodge_the_monsters(MazeGame):
         super().__init__( maze)
         pygame.init()
         self.running=True
+        self.multi2_maze = maze
         self.game_over_FONT = pygame.font.SysFont('comicsans', 100)
         self.game_over_FONT_player = pygame.font.SysFont('comicsans', 50)
         self.heart_sound = pygame.mixer.Sound(os.path.join("Assets","Heal_Sound_Effect_2.wav"))
         self.monster_sound_effect2 = pygame.mixer.Sound(os.path.join("Assets","Pac-Man Death - Sound Effect (HD).wav"))
         self.monster_sound_slap = pygame.mixer.Sound(os.path.join("Assets","SLAP - Sound Effect.wav"))
         self.sound_effect_played = False
+        if self.multi2_maze == Maze_maps.maze_multi2:
+            self.monster4_pos = self.find_character(maze,"K")
         self.monster1_pos=self.find_character(maze,"M")
         self.monster2_pos=self.find_character(maze,"S")
         self.monster3_pos=self.find_character(maze,"R")
         self.heart_pos=self.find_character(maze,"H")
         self.player_health=3
         self.player_2_health = 3
-
-        self.monster1_vel = -0.5
-        self.monster2_vel = -0.5
         self.heart_x = 5 + (self.cell_width + 5) # for heart left player one 
         self.heart2_x = self.window_width-self.cell_width*6
-        self.monster3_vel = 1
-        self.movement_range_M = 11
-        self.movement_range_S = 11
-        self.movement_range_R = 47
-        # moving_the_monsters = self.monster_movement(self.movement_range_M,self.movement_range_S,self.movement_range_R)
-        self.delay_M=pygame.time.get_ticks()
-        self.cooldown_M = 200
-        self.frame_M= 0
-        self.list_M = list_of_frames_blue_monster_back
-        self.delay_S=pygame.time.get_ticks()
-        self.cooldown_S = 200
-        self.frame_S= 0
-        self.list_S = list_of_frames_red_monster_back
-        self.delay_R=pygame.time.get_ticks()
-        self.cooldown_R = 200
-        self.frame_R= 0
-        self.list_R = animation_list_right
         self.frame_heart = 0
+        if self.multi2_maze == Maze_maps.maze_multi2:
+            # monster 1
+            self.monster1_vel = -0.5
+            self.monster_range_M = 9 
+            self.delay_M = pygame.time.get_ticks()
+            self.cooldown_M = 200
+            self.frame_M = 0
+            self.list_M = list_of_frames_blue_monster_back
+            # monster 2
+            self.monster2_vel = -0.5
+            self.monster_range_S = 9 
+            self.delay_S = pygame.time.get_ticks()
+            self.cooldown_S = 200
+            self.frame_S = 0
+            self.list_S = list_of_frames_blue_monster_back
+            # monster 3
+            self.monster3_vel = 1
+            self.monster_range_R = 35 
+            self.delay_R = pygame.time.get_ticks()
+            self.cooldown_R = 200
+            self.frame_R = 0
+            self.list_R = animation_list_right
+            # monster 4
+            self.monster4_vel = 1
+            self.monster_range_K = 35 
+            self.delay_K = pygame.time.get_ticks()
+            self.cooldown_K = 200
+            self.frame_K = 0
+            self.list_K = animation_list_right
+        else:
+            self.monster1_vel = -0.5
+            self.monster2_vel = -0.5
+            self.monster3_vel = 1
+            self.movement_range_M = 11
+            self.movement_range_S = 11
+            self.movement_range_R = 47
+        # moving_the_monsters = self.monster_movement(self.movement_range_M,self.movement_range_S,self.movement_range_R)
+            self.delay_M=pygame.time.get_ticks()
+            self.cooldown_M = 200
+            self.frame_M= 0
+            self.list_M = list_of_frames_blue_monster_back
+            self.delay_S=pygame.time.get_ticks()
+            self.cooldown_S = 200
+            self.frame_S= 0
+            self.list_S = list_of_frames_red_monster_back
+            self.delay_R=pygame.time.get_ticks()
+            self.cooldown_R = 200
+            self.frame_R= 0
+            self.list_R = animation_list_right
         # print(len(self.list_R))
     def draw_hearts(self):
         for row in range(len(self.maze)):
@@ -70,31 +102,67 @@ class Dodge_the_monsters(MazeGame):
         pygame.display.update()
 
     def draw_monster(self, pos_x, pos_y, monster,list):
+        if self.multi2_maze == Maze_maps.maze_multi2:
+            if monster =="M":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_M >= self.cooldown_M:
+                    self.frame_M += 1
+                    self.delay_M = current_time
+                    if self.frame_M >= len(list):
+                        self.frame_M = 0
+                self.window.blit(list[self.frame_M], (pos_x*35+self.window_width//2-(self.cell_width*len(self.maze[0])//2), (pos_y*35)+self.offsett))
+            elif monster == "S":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_S >= self.cooldown_S:
+                    self.frame_S += 1
+                    self.delay_S = current_time
+                    if self.frame_S >= len(list):
+                        self.frame_S = 0
+                self.window.blit(list[self.frame_S], (pos_x*35+self.window_width//2-(self.cell_width*len(self.maze[0])//2), (pos_y*35)+self.offsett))
+            elif monster == "R":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_R >= self.cooldown_R:
+                    self.frame_R += 1
+                    self.delay_R = current_time
+                    if self.frame_R >= len(list):
+                        self.frame_R = 0
+                self.window.blit(list[self.frame_R], (pos_x*35+self.window_width//2-(self.cell_width*len(self.maze[0])//2), pos_y*35+self.offsett))
+            elif monster == "K":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_K >= self.cooldown_K:
+                    self.frame_K += 1
+                    self.delay_K = current_time
+                    if self.frame_K >= len(list):
+                        self.frame_K= 0
+                self.window.blit(list[self.frame_K], (pos_x*35+self.window_width//2-(self.cell_width*len(self.maze[0])//2), pos_y*35+self.offsett))
+        else:
 
-        if monster =="M":
-            current_time = pygame.time.get_ticks()
-            if current_time - self.delay_M >= self.cooldown_M:
-                self.frame_M += 1
-                self.delay_M = current_time
-                if self.frame_M >= len(list):
-                    self.frame_M = 0
-            self.window.blit(list[self.frame_M], (pos_x*35, (pos_y*35)+self.window_width//2-(self.cell_width*len(self.maze[0])//2)+self.offsett))
-        elif monster == "S":
-            current_time = pygame.time.get_ticks()
-            if current_time - self.delay_S >= self.cooldown_S:
-                self.frame_S += 1
-                self.delay_S = current_time
-                if self.frame_S >= len(list):
-                    self.frame_S = 0
-            self.window.blit(list[self.frame_S], (pos_x*35, (pos_y*35)+self.window_width//2-(self.cell_width*len(self.maze[0])//2)+self.offsett))
-        elif monster == "R":
-            current_time = pygame.time.get_ticks()
-            if current_time - self.delay_R >= self.cooldown_R:
-                self.frame_R += 1
-                self.delay_R = current_time
-                if self.frame_R >= len(list):
-                    self.frame_R = 0
-            self.window.blit(list[self.frame_R], (pos_x*35, pos_y*35+self.offsett))
+            if monster =="M":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_M >= self.cooldown_M:
+                    self.frame_M += 1
+                    self.delay_M = current_time
+                    if self.frame_M >= len(list):
+                        self.frame_M = 0
+                self.window.blit(list[self.frame_M], (pos_x*35, (pos_y*35)+self.window_width//2-(self.cell_width*len(self.maze[0])//2)+self.offsett))
+            elif monster == "S":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_S >= self.cooldown_S:
+                    self.frame_S += 1
+                    self.delay_S = current_time
+                    if self.frame_S >= len(list):
+                        self.frame_S = 0
+                self.window.blit(list[self.frame_S], (pos_x*35, (pos_y*35)+self.window_width//2-(self.cell_width*len(self.maze[0])//2)+self.offsett))
+            elif monster == "R":
+                current_time = pygame.time.get_ticks()
+                if current_time - self.delay_R >= self.cooldown_R:
+                    self.frame_R += 1
+                    self.delay_R = current_time
+                    if self.frame_R >= len(list):
+                        self.frame_R = 0
+                self.window.blit(list[self.frame_R], (pos_x*35, pos_y*35+self.offsett))
+        
+
         #     monster_cell_image = pygame.image.load(os.path.join("Assets","green-removebg-preview.png"))
         #     monster_cell_image = pygame.image.load(os.path.join("Assets","blue-removebg-preview.png"))
         #     monster_cell_image = pygame.image.load(os.path.join("Assets","yellow-removebg-preview.png"))
@@ -105,31 +173,67 @@ class Dodge_the_monsters(MazeGame):
 
 
     def monster_movement(self):
-        # monster 1 movement
-        self.monster1_pos[0] += self.monster1_vel
-        if self.monster1_pos[0] <= 14:
-            self.monster1_vel *= -1
-            self.list_M = list_of_frams_blue_monster_front
-        if self.monster1_pos[0] >= 25:
-            self.monster1_vel *= -1
-            self.list_M = list_of_frames_blue_monster_back
-        # monster 2 movement
-        self.monster2_pos[0] += self.monster2_vel
-        if self.monster2_pos[0] <= 14:
-            self.monster2_vel *= -1
-            self.list_S = list_of_frames_red_monster_front
-        if self.monster2_pos[0] >= 25:
-            self.monster2_vel *= -1
-            self.list_S = list_of_frames_red_monster_back
-        
-        # monster 3  movement
-        self.monster3_pos[1] += self.monster3_vel
-        if self.monster3_pos[1] >= 49:
-            self.monster3_vel *= -1
-            self.list_R = animation_list_left
-        if self.monster3_pos[1] <= 2:
-            self.monster3_vel *= -1
-            self.list_R = animation_list_right
+        if self.multi2_maze == Maze_maps.maze_multi2:
+            # monster 1 movement
+            self.monster1_pos[0] += self.monster1_vel
+            if self.monster1_pos[0] <= 14:
+                self.monster1_vel *= -1
+                self.list_M = list_of_frams_blue_monster_front
+            if self.monster1_pos[0] >= 23:
+                self.monster1_vel *= -1
+                self.list_M = list_of_frames_blue_monster_back
+            # monster 2 movement
+            self.monster2_pos[0] += self.monster2_vel
+            if self.monster2_pos[0] <= 14:
+                self.monster2_vel *= -1
+                self.list_S = list_of_frames_red_monster_front
+            if self.monster2_pos[0] >= 23:
+                self.monster2_vel *= -1
+                self.list_S = list_of_frames_red_monster_back
+            
+            # monster 3  movement
+            self.monster3_pos[1] += self.monster3_vel
+            if self.monster3_pos[1] >= 35:
+                self.monster3_vel *= -1
+                self.list_R = animation_list_left
+            if self.monster3_pos[1] <= 2:
+                self.monster3_vel *= -1
+                self.list_R = animation_list_right
+            
+            # monster 4  movement
+            self.monster4_pos[1] += self.monster4_vel
+            if self.monster4_pos[1] >= 35:
+                self.monster4_vel *= -1
+                self.list_K = animation_list_left
+            if self.monster4_pos[1] <= 2:
+                self.monster4_vel *= -1
+                self.list_K = animation_list_right
+        else:
+            # monster 1 movement
+            self.monster1_pos[0] += self.monster1_vel
+            if self.monster1_pos[0] <= 14:
+                self.monster1_vel *= -1
+                self.list_M = list_of_frams_blue_monster_front
+            if self.monster1_pos[0] >= 25:
+                self.monster1_vel *= -1
+                self.list_M = list_of_frames_blue_monster_back
+            # monster 2 movement
+            self.monster2_pos[0] += self.monster2_vel
+            if self.monster2_pos[0] <= 14:
+                self.monster2_vel *= -1
+                self.list_S = list_of_frames_red_monster_front
+            if self.monster2_pos[0] >= 25:
+                self.monster2_vel *= -1
+                self.list_S = list_of_frames_red_monster_back
+            
+            # monster 3  movement
+            self.monster3_pos[1] += self.monster3_vel
+            if self.monster3_pos[1] >= 49:
+                self.monster3_vel *= -1
+                self.list_R = animation_list_left
+            if self.monster3_pos[1] <= 2:
+                self.monster3_vel *= -1
+                self.list_R = animation_list_right
 
     def game_over(self,multi):
         # print(self.player_health , self.player_2_health)
@@ -183,23 +287,45 @@ class Dodge_the_monsters(MazeGame):
 
 
     def health_points_count(self):
-        if (self.player_pos[0] == self.monster1_pos[0] and self.player_pos[1] == self.monster1_pos[1]) or \
-        (self.player_pos[0] == self.monster2_pos[0] and self.player_pos[1] == self.monster2_pos[1]) or \
-        (self.player_pos[0] == self.monster3_pos[0] and self.player_pos[1] == self.monster3_pos[1]):
-            if self.player_health > 0:
-                self.player_health -= 1
-                self.monster_sound_slap.play()
-        if (self.player_2_pos[0] == self.monster1_pos[0] and self.player_2_pos[1] == self.monster1_pos[1]) or \
-            (self.player_2_pos[0] == self.monster2_pos[0] and self.player_2_pos[1] == self.monster2_pos[1]) or \
-            (self.player_2_pos[0] == self.monster3_pos[0] and self.player_2_pos[1] == self.monster3_pos[1]):
-            if self.player_2_health > 0:
-                self.player_2_health -= 1
-                self.monster_sound_slap.play()
+        if self.multi2_maze == Maze_maps.maze_multi2:
+            if (self.player_pos[0] == self.monster1_pos[0] and self.player_pos[1] == self.monster1_pos[1]) or \
+            (self.player_pos[0] == self.monster2_pos[0] and self.player_pos[1] == self.monster2_pos[1]) or \
+            (self.player_pos[0] == self.monster3_pos[0] and self.player_pos[1] == self.monster3_pos[1]) or \
+            (self.player_pos[0] == self.monster4_pos[0] and self.player_pos[1] == self.monster4_pos[1]):
+                if self.player_health > 0:
+                    self.player_health -= 1
+                    self.monster_sound_slap.play()
+            if (self.player_2_pos[0] == self.monster1_pos[0] and self.player_2_pos[1] == self.monster1_pos[1]) or \
+                (self.player_2_pos[0] == self.monster2_pos[0] and self.player_2_pos[1] == self.monster2_pos[1]) or \
+                (self.player_2_pos[0] == self.monster3_pos[0] and self.player_2_pos[1] == self.monster3_pos[1]) or \
+                (self.player_2_pos[0] == self.monster4_pos[0] and self.player_2_pos[1] == self.monster4_pos[1])    :
+                if self.player_2_health > 0:
+                    self.player_2_health -= 1
+                    self.monster_sound_slap.play()
 
-        if self.player_2_health == 0:
-            self.maze[self.player_2_pos[0]][self.player_2_pos[1]] = " "
-        if self.player_health == 0:
-            self.maze[self.player_pos[0]][self.player_pos[1]] = " "
+            if self.player_2_health == 0:
+                self.maze[self.player_2_pos[0]][self.player_2_pos[1]] = " "
+            if self.player_health == 0:
+                self.maze[self.player_pos[0]][self.player_pos[1]] = " "
+        else:
+
+            if (self.player_pos[0] == self.monster1_pos[0] and self.player_pos[1] == self.monster1_pos[1]) or \
+            (self.player_pos[0] == self.monster2_pos[0] and self.player_pos[1] == self.monster2_pos[1]) or \
+            (self.player_pos[0] == self.monster3_pos[0] and self.player_pos[1] == self.monster3_pos[1]):
+                if self.player_health > 0:
+                    self.player_health -= 1
+                    self.monster_sound_slap.play()
+            if (self.player_2_pos[0] == self.monster1_pos[0] and self.player_2_pos[1] == self.monster1_pos[1]) or \
+                (self.player_2_pos[0] == self.monster2_pos[0] and self.player_2_pos[1] == self.monster2_pos[1]) or \
+                (self.player_2_pos[0] == self.monster3_pos[0] and self.player_2_pos[1] == self.monster3_pos[1]):
+                if self.player_2_health > 0:
+                    self.player_2_health -= 1
+                    self.monster_sound_slap.play()
+
+            if self.player_2_health == 0:
+                self.maze[self.player_2_pos[0]][self.player_2_pos[1]] = " "
+            if self.player_health == 0:
+                self.maze[self.player_pos[0]][self.player_pos[1]] = " "
 
     
 
@@ -256,6 +382,9 @@ class Dodge_the_monsters(MazeGame):
             self.draw_monster(self.monster1_pos[1],self.monster1_pos[0],"M",self.list_M)
             self.draw_monster(self.monster2_pos[1],self.monster2_pos[0],"S",self.list_S)
             self.draw_monster(self.monster3_pos[1],self.monster3_pos[0],"R",self.list_R)
+            if self.multi2_maze == Maze_maps.maze_multi2:
+                self.draw_monster(self.monster4_pos[1],self.monster4_pos[0],"K",self.list_K)
+
             self.draw_hearts()
             self.draw_hearts_points(self.player_health,self.heart_x)
             if multi:
@@ -271,10 +400,10 @@ class Dodge_the_monsters(MazeGame):
                 self.running = False
         # pygame.quit()
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    # Create an instance of the MazeGame class
-    game = Dodge_the_monsters( Maze_maps.maze_multi)
+#     # Create an instance of the MazeGame class
+#     game = Dodge_the_monsters( Maze_maps.maze_multi)
 
-    # Run the game
-    game.run(True)
+#     # Run the game
+#     game.run(True)
